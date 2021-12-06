b41e123d1793d539be26026b74bdb9308a5d710f
Kedarbhat895 <58362637+Kedarbhat895@users.noreply.github.com>
2021-12-06 21:57:09 +0530

    Add files via upload

--- file1.py
+++ file1.py
@@ -4,7 +4,7 @@ from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import col
 import re
-import f2,model
+import f2,model,test
 
 
 sc=SparkContext.getOrCreate()
@@ -18,27 +18,36 @@ except Exception as e:
 	print(e)
 
 def readstream(rdd):
-	try:
-		df=spark.read.json(rdd)
-	except Exception as e:
-		print(e)
-	newcols = [col(column).alias(re.sub('\s*', '', column)) \
-	for column in df.columns]
+	if(not rdd.isEmpty()):
+		try:
+			df=spark.read.json(rdd)
+		except Exception as e:
+			print(e)
+		newcols = [col(column).alias(re.sub('\s*', '', column)) \
+		for column in df.columns]
 	
-	
-	try:
-		df1=f2.f1(df)
-		df2=f2.f2(df1)
-		df3=f2.lemma(df2)
-		df4=f2.text_preprocess(df3)
-		df4.select("combined_F","label").show(truncate=False)
-	except Exception as e:
-		print(e)
 		
-	try:
-		#model.f1(df4)
-	except Exception as e:
-		print(e)
+		try:
+			df1=f2.f1(df)
+			df2=f2.f2(df1)
+			df3=f2.lemma(df2)
+			df4=f2.text_preprocess(df3)
+			#df4.select("combined_F","label").show(truncate=True)
+		except Exception as e:
+			print(e)
+			
+		try:
+			x,y,a,b=model.split(df4)
+			model.f1(x,y,a,b)
+			#model.cluster(x,y,a,b)
+		except Exception as e:
+			print(e)
+			
+		'''try:
+			test.f1(df4)
+		except Exception as e:
+			print(e)'''
+		
 		
 		
 		
--- model.py
+++ model.py
@@ -4,24 +4,24 @@ import ast
 from operator import attrgetter
 from scipy.sparse import vstack,csr_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,classification_report
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDClassifier,PassiveAggressiveClassifier,Perceptron
+from sklearn.cluster import MiniBatchKMeans
 
 clf=MultinomialNB(alpha=0.01)
 clf2=SGDClassifier(max_iter=5)
 clf3=PassiveAggressiveClassifier()
 clf4=Perceptron()
+clf5=MiniBatchKMeans()
 
 
 def as_mat(vec):
  	data,ind=vec.values,vec.indices
  	shape=1,vec.size
  	return csr_matrix((data,ind,np.array([0,vec.values.size])),shape)
-
-
-
-def f1(df):
+ 	
+def split(df):
 	X=df.select('combined_F')
 	y=df.select('label')
 	y=np.array(y.collect()).T
@@ -35,10 +35,13 @@ def f1(df):
 	
 	X_train,X_test,y_train,y_test=train_test_split(mat,y,test_size=0.1,random_state=42)
 	
+	return X_train,y_train,X_test,y_test
 	
-	#print(mat)
-	
-	
+
+
+
+def f1(X_train,y_train,X_test,y_test):
+
 	clf.partial_fit(X_train, y_train, classes=[0.0,1.0])
 	clf2.partial_fit(X_train,y_train,classes=[0.0,1.0])
 	clf3.partial_fit(X_train,y_train,classes=[0.0,1.0])
@@ -47,10 +50,14 @@ def f1(df):
 	b=clf2.predict(X_test)
 	c=clf3.predict(X_test)
 	d=clf4.predict(X_test)
-	print("accuracy using MultinomialNB is:",accuracy_score(a,y_test))
+	print("Report MultinomialNB\n:",classification_report(a,y_test,labels=[0.0,1.0]))
+	print("Report SGDC\n:",classification_report(b,y_test,labels=[0.0,1.0]))
+	print("Report PAC\n:",classification_report(c,y_test,labels=[0.0,1.0]))
+	print("Report Perceptron\n:",classification_report(d,y_test,labels=[0.0,1.0]))
+	'''print("accuracy using MultinomialNB is:",accuracy_score(a,y_test))
 	print("accuracy using SGDClassifier is:",accuracy_score(b,y_test))
 	print("accuracy using PassiveAggressiveClassifier is:",accuracy_score(c,y_test))
-	print("accuracy using Perceptron() is:",accuracy_score(d,y_test))
+	print("accuracy using Perceptron() is:",accuracy_score(d,y_test))'''
 	
 	try:
 		with open('MNB','wb') as file:
@@ -64,12 +71,24 @@ def f1(df):
 		with open('percep.pkl','wb') as file:
 			pickle.dump(clf4,file)
 			
-		print("DONE!")
+		#print("DONE!")
 		
 	except Exception as e:
 		print(e)
 		
 	return X_test,y_test
+	
+	
+def cluster(X_train,y_train,X_test,y_test):
+	
+	clf5.partial_fit(X_train,y_train)
+	y_pred=clf5.predict(X_test)
+	print("Accuracy using clustering: ",accuracy_score(y_pred,y_test))
+		
+	
+	
+	
+	
 		
 	
 	
